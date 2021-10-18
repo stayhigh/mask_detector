3a9698fe19419d088fc077bf22d057362c9abd1a
stayhigh <redhero0702@gmail.com>
2021-10-18 17:39:05 +0800

    remove FPS class implementation

--- webcamfps.py
+++ webcamfps.py
@@ -21,38 +21,6 @@ ap.add_argument("--height", type=int, default=720,
 args = vars(ap.parse_args())
 
 
-class FPS:
-    def __init__(self):
-        # store the start time, end time, and total number of frames
-        # that were examined between the start and end intervals
-        self._start = None
-        self._end = None
-        self._numFrames = 0
-
-    def start(self):
-        # start the timer
-        self._start = datetime.datetime.now()
-        return self
-
-    def stop(self):
-        # stop the timer
-        self._end = datetime.datetime.now()
-
-    def update(self):
-        # increment the total number of frames examined during the
-        # start and end intervals
-        self._numFrames += 1
-
-    def elapsed(self):
-        # return the total number of seconds between the start and
-        # end interval
-        return (self._end - self._start).total_seconds()
-
-    def fps(self):
-        # compute the (approximate) frames per second
-        return self._numFrames / self.elapsed()
-
-
 def set_camera_resolution_for_normal_fps(camera, width=args['width'], height=args['height']):
     print('setting video resolution:{:.0f}x{:.0f}'.format(width, height))
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, width)
