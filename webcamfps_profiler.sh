bbfc4efc8a62b4a7e8892f90fe25276e2691a9f0
stayhigh <redhero0702@gmail.com>
2021-10-19 20:19:20 +0800

    update profiling script for webcamfps.py

--- webcamfps_profiler.sh
+++ webcamfps_profiler.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+set -x
+
+# set conda env
+CONDA_PATH=$(conda info | grep -i 'base environment' | awk '{print $4}') # brew install coreutils
+source $CONDA_PATH/etc/profile.d/conda.sh
+conda activate mask_check
+
 # profile the script
-kernprof -l -v webcamfps.py
+python webcamfps.py
+
+# run kernprof if you want to use decorator @profile
+# kernprof -l -v webcamfps.py
 
 # show the profile
 python -m line_profiler  webcamfps.py.lprof
