fa50d9a3372cc125543cc6cd9ed9e228641a0279
stayhigh <redhero0702@gmail.com>
2021-10-19 21:20:58 +0800

    add timeout and max limit of frame for webcam_demo.py

--- webcam_demo.py
+++ webcam_demo.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python3
+import sys
 import cv2
 import numpy as np
 import tensorflow as tf
 from mtcnn import MTCNN
 from imutils.video import WebcamVideoStream  # threaded version
+from imutils.video import FPS
+from line_profiler import LineProfiler
+import argparse
 
-MASK_MODEL_ENABLE = True
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--timeout", type=int, default=30,
+                help="timeout (seconds)")
+ap.add_argument("-n", "--num-frames", type=int, default=float("inf"),
+                help="# of frames to loop over for FPS test")
+args = vars(ap.parse_args())
+
+MASK_MODEL_ENABLE = False
 
 SOURCE = 0  # or .mp4 file path
 INPUT_SIZE = (112, 112) 
 MODEL_PATH = 'exported/'
 
 
-
+# @profile
 def crop_img(end_x, end_y, frame, start_x, start_y):
     face_img = frame[start_y:end_y, start_x:end_x, :]
-    print ("INPUT_SIZE:", INPUT_SIZE)
-    print ("face_img:", face_img)
+    # print ("INPUT_SIZE:", INPUT_SIZE)
+    # print ("face_img:", face_img)
     try:
         face_img = cv2.resize(face_img, INPUT_SIZE)
     except cv2.error as e:
@@ -25,7 +38,7 @@ def crop_img(end_x, end_y, frame, start_x, start_y):
     face_img = face_img * 0.0078125
     return face_img
 
-
+# @profile
 def draw_bbox(frame, start_x, start_y, end_x, end_y, have_mask):
     # frame now is RGB
     #color = (0, 255, 0) if have_mask else (255, 0, 0)
@@ -47,7 +60,7 @@ def draw_bbox(frame, start_x, start_y, end_x, end_y, have_mask):
         cv2.putText(frame, 'no_mask', (start_x, start_y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
     return frame
 
-
+# @profile
 def main():
     face_detector = MTCNN()
     if MASK_MODEL_ENABLE:
@@ -55,15 +68,23 @@ def main():
 
     # non-threaded source:    cap = cv2.VideoCapture(SOURCE)
     vs = WebcamVideoStream(src=SOURCE).start()
+    fps = FPS().start()
     cap = vs.stream
 
     have_mask = False # here
-    while True:
+
+    while fps._numFrames < args["num_frames"]:
+        print('num of frames: {}'.format(fps._numFrames))
+        if getattr(fps, '_end') and fps.elapsed() > args["timeout"]:
+            print("timeout {} seconds".format(args["timeout"]))
+            break
         ret, frame = cap.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # face detector is too slow
+        lprofiler.add_function(face_detector.detect_faces)
         face_locs = face_detector.detect_faces(frame)
+
         for face_loc in face_locs:
             bbox = face_loc['box']
             start_x = bbox[0]
@@ -92,5 +113,26 @@ def main():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+        fps.update()
+        fps.stop()
+
+    # stop the timer and display FPS information
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+
 if __name__ == '__main__':
-    main()
+    lprofiler = LineProfiler()
+    lprofiler.add_function(main)
+    lprofiler.add_function(crop_img)
+    lprofiler.add_function(draw_bbox)
+
+    lp_wrapper = lprofiler(main)
+    lp_wrapper()
+    statfile = "{}.lprof".format(sys.argv[0])
+    lprofiler.dump_stats(statfile)
+    # main()
